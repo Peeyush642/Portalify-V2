101111832009e33fef36c7e8b6ff71b8d151ffb3
Peeyush Kumar <peeyush642@gmail.com>
2024-06-14 19:26:43 +0530

    page error fix

--- src/app/formbuilder/template/page.js
+++ src/app/formbuilder/template/page.js
@@ -1,7 +1,7 @@
 "use client";
-import Navbar from "../../../components/Formbuilder/Navbar/navbar";
-import FormBuilder from "../../../components/FormBuilder/FormBuilder";
-import FormBuilderContext from "../../../context/FormBuilderContext";
+import Navbar from "@/components/Formbuilder/Navbar/navbar";
+import FormBuilder from "@/components/FormBuilder/FormBuilder";
+import FormBuilderContext from "@/context/FormBuilderContext";
 import { useRouter } from 'next/router';
 export default function Page()  {
   return (
--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -8,10 +8,10 @@ import { useRouter } from "next/navigation";
 
 const Dashboard = ({ content }) => {
   const router = useRouter();
-  let isAuthenticated = "";
-  if (typeof window !== "undefined") {
-    isAuthenticated = localStorage.getItem("isAuthenticated") || "";
-  }
+   let isAuthenticated = "";
+    if (typeof window !== "undefined") {
+        isAuthenticated = localStorage.getItem("isAuthenticated");
+    }
   
   useEffect(() => {
     if (!isAuthenticated) {
