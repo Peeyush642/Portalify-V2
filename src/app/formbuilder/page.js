913fe00e20436bcd02da96205be224620c8b1e81
Peeyush Kumar <peeyush642@gmail.com>
2024-06-15 16:16:47 +0530

    Naming error fix

--- src/app/formbuilder/page.js
+++ src/app/formbuilder/page.js
@@ -1,6 +1,6 @@
 "use client";
-import Navbar from "@/components/FormBuilder/Navbar/navbar";
-import FormBuilder from "@/components/Formbuilder/formBuilder";
+import Navbar from "@/components/Formbuilder/FormNavbar/navbar";
+import FormBuilder from "@/components/Formbuilder/FormBuilderC";
 import FormBuilderContext from "@/context/FormBuilderContext";
 import { useRouter } from 'next/router';
 export default function Page()  {
--- src/app/formbuilder/template/page.js
+++ src/app/formbuilder/template/page.js
@@ -1,6 +1,6 @@
 "use client";
-import Navbar from "@/components/Formbuilder/Navbar/navbar";
-import FormBuilder from "@/components/FormBuilder/FormBuilder";
+import Navbar from "@/components/FormBuilder/FormNavbar/navbar";
+import FormBuilder from "@/components/FormBuilder/FormBuilderC";
 import FormBuilderContext from "@/context/FormBuilderContext";
 import { useRouter } from 'next/router';
 export default function Page()  {
